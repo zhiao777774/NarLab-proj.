51992840dd1f1d77f167d5ffe27d847adffd8d85
zhiao777774 <zhiaohsu@gmail.com>
2022-12-22 21:27:17 +0800

    update: renaming file and adding PCA

--- cluster_visualization.py
+++ cluster_visualization.py
@@ -1,7 +1,8 @@
 import argparse
-import numpy as np
 import pickle
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -35,27 +36,28 @@ def keep_description(dataset, language):
     return np.array(data, dtype=float)
 
 
-def display_scatter(vectors, dim=2):
+def display_scatter(vectors, dim=2, alg=0):
     word_vectors = np.array([v['data'] for v in vectors])
     labels = [v['label'] for v in vectors]
-    embedded = TSNE(n_components=dim).fit_transform(word_vectors)[:, :dim]
+
+    model = PCA(n_components=dim) if alg else TSNE(n_components=dim)
+    projection_matrix = model.fit_transform(word_vectors)[:, :dim]
 
     fig = plt.figure(figsize=(6, 6))
     if dim == 3:
         ax = Axes3D(fig)
-        for l in set(labels):
-            x = [embedded[i, 0] for i in range(len(embedded)) if labels[i] == l]
-            y = [embedded[i, 1] for i in range(len(embedded)) if labels[i] == l]
-            z = [embedded[i, 2] for i in range(len(embedded)) if labels[i] == l]
-            ax.plot(x, y, z, 'o', label=l, markeredgecolor='black')
+        for label in set(labels):
+            x = [projection_matrix[i, 0] for i in range(len(projection_matrix)) if labels[i] == label]
+            y = [projection_matrix[i, 1] for i in range(len(projection_matrix)) if labels[i] == label]
+            z = [projection_matrix[i, 2] for i in range(len(projection_matrix)) if labels[i] == label]
+            ax.plot(x, y, z, 'o', label=label, markeredgecolor='black')
     elif dim == 2:
-        for l in set(labels):
-            x = [embedded[i, 0] for i in range(len(embedded)) if labels[i] == l]
-            y = [embedded[i, 1] for i in range(len(embedded)) if labels[i] == l]
-            plt.plot(x, y, 'o', label=l, markeredgecolor='black')
+        for label in set(labels):
+            x = [projection_matrix[i, 0] for i in range(len(projection_matrix)) if labels[i] == label]
+            y = [projection_matrix[i, 1] for i in range(len(projection_matrix)) if labels[i] == label]
+            plt.plot(x, y, 'o', label=label, markeredgecolor='black')
 
-    plt.tick_params(left=False, right=False, labelleft=False,
-                    labelbottom=False, bottom=False)
+    plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
     plt.legend(bbox_to_anchor=(0.95, 0.99))
 
     plt.savefig(nar.util.DATA_PATH + f'/{dim}d_tsne_cluster.png')
@@ -70,13 +72,13 @@ if __name__ == '__main__':
     labeled_path = f'{nar.util.DATA_PATH}/xlsx_description_{args.language}.pickle'
     data = load_data(dataset_path)
     label = load_data(labeled_path)
-    dataset = keep_description(data, 'chinese')
+    data = keep_description(data, 'chinese')
 
-    mapping = []
-    for l, d in zip(label.values(), dataset):
-        mapping.append({
+    dataset = []
+    for l, d in zip(label.values(), data):
+        dataset.append({
             'data': d,
             'label': l['label']
         })
 
-    display_scatter(mapping)
+    display_scatter(dataset)
