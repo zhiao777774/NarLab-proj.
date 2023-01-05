90eaa5d6e574b2b993995d56121ada038e932d30
zhiao777774 <zhiaohsu@gmail.com>
2023-01-05 11:27:28 +0800

    add: extending Date class method

--- pea-sys/src/index.tsx
+++ pea-sys/src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {diffDay, diffYear, toRepublicYear, toCE} from "./utils/date";
+
+
+// @ts-ignore
+Date.prototype.diffDay = diffDay;
+// @ts-ignore
+Date.prototype.diffYear = diffYear;
+// @ts-ignore
+Date.prototype.toRepublicYear = toRepublicYear;
+// @ts-ignore
+Date.prototype.toCE = toCE;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
