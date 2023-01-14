fd77c9a6863e5864ec25621fb090bc51c8a27250
zhiao777774 <zhiaohsu@gmail.com>
2023-01-15 05:40:46 +0800

    update: correct multi-label problem

--- pea-sys/src/helpers/dataLoader.tsx
+++ pea-sys/src/helpers/dataLoader.tsx
@@ -1,7 +1,8 @@
 import category from '../constants/category';
-import {numberRange} from "../utils/range";
+import {Task} from '../constants/taskPropType';
+import {numberRange} from '../utils/range';
 
-const color_list = [
+const COLOR_LIST = [
     '#010221', '#010221', '#010221', '#0A7373',
     '#0A7373', '#0A7373', '#B7BF99', '#B7BF99',
     '#B7BF99', '#EDAA25', '#EDAA25', '#EDAA25',
@@ -9,16 +10,16 @@ const color_list = [
     '#C43302', '#C43302'
 ];
 
-const projectColor = '#0A7373';
-const taskColor = '#2E4E78';
+const PROJECT_COLOR = '#0A7373';
+const TASK_COLOR = '#2E4E78';
 
-export function loadData(catNum: string = '10') {
+export function loadData(catNum: string = '10'): Task[] {
     const projectData = require(`../data/revised/dataset.json`);
     const catSeries = require(`../data/revised/category_statistic.json`);
-    const yearRange = numberRange(103, 110, true)
+    const catProb = require(`../data/revised/category_probability.json`);
 
-    const tasks: object[] = [];
-
+    const tasks: Task[] = [];
+    const yearRange = numberRange(103, 110, true);
     for (let i = 0; i < category.length; ++i) {
         // @ts-ignore
         const start = new Date(103, 0, 1).toCE();
@@ -27,21 +28,21 @@ export function loadData(catNum: string = '10') {
 
         const temp = {
             start,
-            start_date: `${start.getFullYear()}-1-1`,
             end,
+            start_date: `${start.getFullYear()}-1-1`,
             duration: end.diffYear(start),
             name: category[i],
             text: category[i],
             id: `main_${i}`,
             level: 1,
-            type: "project",
+            type: 'project',
             progress: 1,
-            color: projectColor, //color_list[i % color_list.length],
+            color: PROJECT_COLOR,
             data: {
-                years: yearRange,
+                years: yearRange.map((n) => String(Math.round(n))),
                 series: yearRange.map((year) => catSeries[category[i]][year] || 0)
             }
-        };
+        } as Task;
 
         tasks.push(temp);
     }
@@ -54,31 +55,35 @@ export function loadData(catNum: string = '10') {
         // @ts-ignore
         const end = new Date(proj.endDate, 0, 1).toCE();
 
-        const parent = `main_${category.indexOf(proj.category.split(',')[0])}`;
-        const temp = {
+        // TODO: 目前多標籤只取第一個，可能要做修改
+        const catIndex = category.indexOf(proj.category.split(';')[0]);
+        const parent = `main_${catIndex}`;
+        const project = {
             start,
-            start_date: `${start.getFullYear()}-1-1`,
             end,
+            start_date: `${start.getFullYear()}-1-1`,
             duration: end.diffYear(start),
             name: proj.name,
             text: proj.name,
             id: `proj_${i}`,
             level: 2,
-            type: "task",
+            type: 'task',
             project: parent,
             parent,
             progress: 1,
-            color: taskColor,
+            color: TASK_COLOR,
             data: {
-                "keyword": proj.chineseKeyword,
-                //"ner": proj.ner,
-                //"tf_idf": proj.tf_idf,
-                "desp": proj.description,
-                "department": proj.department,
+                keyword: proj.chineseKeyword,
+                desp: proj.description.replaceAll('_x000D_', '\n'),
+                department: proj.department,
+                category: catProb[proj.code]['predictCategoryTop5'].split(';'),
+                categoryProb: catProb[proj.code]['predictProbabilityTop5'].split(';')
+                    .map((prob: string) => Number(prob))
             }
-        };
+        } as Task;
 
-        tasks.push(temp);
+        tasks.push(project);
     }
+
     return tasks;
 }
