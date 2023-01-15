a17cebe96693d79f3bdc3075c84de6fe3955bb6a
zhiao777774 <zhiaohsu@gmail.com>
2023-01-16 07:26:53 +0800

    add: new context variable for sidebar collasped status

--- pea-sys/src/helpers/CategoryContext.tsx
+++ pea-sys/src/helpers/CategoryContext.tsx
@@ -1,3 +1,5 @@
-import { createContext } from 'react';
+import {createContext} from 'react';
 
 export const CategoryContext = createContext("");
+
+export const SidebarCollapsedContext = createContext(false);
