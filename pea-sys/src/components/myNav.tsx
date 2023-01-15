886e25f950d7055b2a724aec7901aff9e5b63a48
zhiao777774 <zhiaohsu@gmail.com>
2023-01-16 07:33:48 +0800

    update: reformat code

--- pea-sys/src/components/myNav.tsx
+++ pea-sys/src/components/myNav.tsx
@@ -1,19 +1,18 @@
 import {Container, Navbar, Nav} from 'react-bootstrap';
 
-export const MyNav=()=>{
+export const MyNav = () => {
     return (
-        <Navbar bg="dark" expand="lg" >
+        <Navbar bg="dark" expand="lg">
             <Container>
-            <Navbar.Brand className="text-light">
-                計畫演變分析系統
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand className="text-light">
+                    計畫演變分析系統
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-} 
-
+};
