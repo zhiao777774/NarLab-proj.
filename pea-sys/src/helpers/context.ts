5e08875274b6f2bc90d54150bfcd2ad1534cc8e2
zhiao777774 <zhiaohsu@gmail.com>
2023-01-19 02:50:05 +0800

    add: add context for popup-panel status

--- pea-sys/src/helpers/context.ts
+++ pea-sys/src/helpers/context.ts
@@ -3,3 +3,8 @@ import {createContext} from 'react';
 export const CategoryContext = createContext('');
 
 export const SidebarCollapsedContext = createContext(false);
+
+export const SearchPopupPanelContext = createContext({
+    openPanel: false,
+    setOpenPanel: (openPanel: boolean) => {}
+});
