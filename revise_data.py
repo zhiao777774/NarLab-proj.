0cdea8710cbd18ed02c62d9ba6e830ff8838caa9
zhiao777774 <zhiaohsu@gmail.com>
2023-02-23 17:15:24 +0800

    update: revise data

--- revise_data.py
+++ revise_data.py
@@ -18,13 +18,14 @@ df = df.dropna(how='all')
 # df = df.astype({'startDate': int})
 df = df[df.columns.drop(list(df.filter(regex='None')))]
 pattern = re.compile(r'\(\d/\d\)')
+df['name'] = df['name'].str.replace(':', '：').replace('（', '(').replace('）', ')')
+df['name'] = df.apply(lambda x: re.sub(r'\s+', '', str(x['name'])), axis=1)
 df['project'] = df.apply(lambda x: pattern.sub('', str(x['name'])), axis=1)
-df['name'] = df['name'].str.replace(':', '：')
 df = df.sort_values(by=['project', 'startDate', 'name'])
 
 end_dates = []
 prev_project = None
-combined_data = df.set_index('project').replace(np.nan, ' ').groupby(level=0).apply(lambda x: x.to_dict('records')).to_dict()
+combined_data = df.set_index('project').replace(np.nan, ' ')
 for i, row in df.iterrows():
     name = pattern.sub('', str(row['name']))
     # if name not in combined_data:
@@ -34,14 +35,16 @@ for i, row in df.iterrows():
         prev_name = pattern.sub('', str(prev_project['name']))
         if prev_name == name:
             end_dates.append(row['startDate'])
-            # combined_data[name].append(row.to_dict())
         else:
-            end_dates.append(110)
+            end_dates.append(prev_project['startDate'])
 
     prev_project = row
 
-end_dates.append(110)
-df['endDate'] = end_dates
+end_dates.append(prev_project['startDate'])
+combined_data['endDate'] = end_dates
+combined_data = combined_data.dropna()
+combined_data = combined_data.astype({'startDate': 'int64', 'endDate': 'int64'})
 
-with open(f'{nar.util.XML_PATH}/dataset_combine.json', 'w', encoding='UTF-8') as f:
-    json.dump(combined_data, f, ensure_ascii=False)
+with open(f'{nar.util.XML_PATH}/dataset_combine2.json', 'w', encoding='UTF-8') as f:
+    json.dump(combined_data.groupby(level=0).apply(
+        lambda x: x.to_dict('records')).to_dict(), f, ensure_ascii=False)
