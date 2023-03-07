56f0181482bab96787ef8e6b9e0cd719befceb41
zhiao777774 <zhiaohsu@gmail.com>
2023-03-07 10:50:59 +0800

    update: revised code

--- pea-sys/src/components/InfoPanel.tsx
+++ pea-sys/src/components/InfoPanel.tsx
@@ -9,20 +9,24 @@ export const InfoPanel: React.FC<{ task: Task; setCurTask: Function; }> = ({task
     const [phase, setPhase] = useState<number>(0);
     if (task === undefined) return null;
 
+    const isTask = (task.level === 3 && task.type === 'task');
+    const target = isTask ? task : task.data[phase];
+
     return (
         <Row className={styles.infoPanelContainer}>
             <Row>
                 <CloseButton onClick={() => setCurTask(null)}/>
             </Row>
             <Row style={{padding: '5px', textAlign: 'center', marginTop: '8px', marginBottom: '5px'}}>
-                <h4>{task.data[phase].name}</h4>
+                <h4>{target.name}</h4>
             </Row>
             <Row>
                 {
-                    task.data.length > 1 ?
+                    (!isTask && task.data.length > 1) ?
                         <InputGroup className="mb-4 justify-content-center">
                             {task.data.map((t: any, i: number) =>
-                                <Button key={`${t.name}-${i}`} variant="outline-dark" size="sm" onClick={() => setPhase(i)}>
+                                <Button key={`${t.name}-${i}`} variant="outline-dark" size="sm"
+                                        onClick={() => setPhase(i)}>
                                     第 {i + 1} 期
                                 </Button>
                             )}
@@ -31,18 +35,18 @@ export const InfoPanel: React.FC<{ task: Task; setCurTask: Function; }> = ({task
                 }
             </Row>
             <Col xs={7}>
-                <h5>年度 </h5> <p>{task.data[phase].start.toRepublicYear().getFullYear()}</p>
-                <h5>部會 </h5> <p>{task.data[phase].data.department}</p>
+                <h5>年度 </h5> <p>{target.start.toRepublicYear().getFullYear()}</p>
+                <h5>部會 </h5> <p>{target.data.department}</p>
                 <h5>計畫描述</h5>
                 <div className={styles.infoPanelDesp}>
-                    <p>{task.data[phase].data.description}</p>
+                    <p>{target.data.description}</p>
                 </div>
             </Col>
             <Col>
                 {
                     <div className={styles.infoPanelTag}>
                         {
-                            task.data[phase].data.keyword && (
+                            target.data.keyword && (
                                 <Alert variant="warning">
                                     <Alert.Heading>
                                         <AiOutlineKey className="me-1"/>
@@ -50,10 +54,10 @@ export const InfoPanel: React.FC<{ task: Task; setCurTask: Function; }> = ({task
                                     </Alert.Heading>
                                     <hr/>
                                     <div className="mb-0 text-black overflow-auto">
-                                        {task.data[phase].data.keyword.split(/[;,，、]/).map((kw: string, i: number) => {
+                                        {target.data.keyword.split(/[;,，、]/).map((kw: string, i: number) => {
                                                 if (!kw.trim()) return null;
                                                 return (
-                                                    <div key={`${task.data[phase].id}_keyword_${i}`}
+                                                    <div key={`${target.id}_keyword_${i}`}
                                                          className="d-inline-block rounded-2 bg-warning bg-opacity-50 py-1 px-2 m-2">{kw}</div>
                                                 );
                                             }
@@ -70,15 +74,15 @@ export const InfoPanel: React.FC<{ task: Task; setCurTask: Function; }> = ({task
                             <hr/>
                             <div className="mb-0 text-black overflow-auto">
                                 {
-                                    task.data[phase].data.tfidf.CH.map((tfidf: string, i: number) =>
-                                        <div key={`${task.data[phase].id}_tfidf_ch${i}`}
+                                    target.data.tfidf.CH.map((tfidf: string, i: number) =>
+                                        <div key={`${target.id}_tfidf_ch${i}`}
                                              className="d-inline-block rounded-2 bg-info bg-opacity-50 py-1 px-2 m-2">{tfidf}</div>
                                     )
                                 }
                             </div>
                         </Alert>
                         {
-                            task.data[phase].data.tfidf.EN.length ?
+                            target.data.tfidf.EN.length ?
                                 <Alert variant="danger">
                                     <Alert.Heading>
                                         <HiDocumentMagnifyingGlass className="me-1"/>
@@ -87,7 +91,7 @@ export const InfoPanel: React.FC<{ task: Task; setCurTask: Function; }> = ({task
                                     <hr/>
                                     <div className="mb-0 text-black overflow-auto">
                                         {
-                                            task.data[phase].data.tfidf.EN.map((tfidf: string, i: number) =>
+                                            target.data.tfidf.EN.map((tfidf: string, i: number) =>
                                                 <div key={`${task.id}_tfidf_en${i}`}
                                                      className="d-inline-block rounded-2 bg-danger bg-opacity-50 py-1 px-2 m-2">{tfidf}</div>
                                             )
