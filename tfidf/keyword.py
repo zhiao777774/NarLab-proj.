65d2dfe545735b7fbca00a54a939624a9be30ac0
hedy881028 <hedy881028.cs07@nctu.edu.tw>
2023-03-13 22:52:42 +0800

    fix: add main

--- tfidf/keyword.py
+++ tfidf/keyword.py
@@ -5,8 +5,13 @@ from ckiptagger import WS, POS
 from sklearn.feature_extraction.text import TfidfVectorizer
 import re
 import json
+from argparse import ArgumentParser
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
+ws = WS('./tfidf/data', disable_cuda=False)
+pos = POS('./tfidf/data', disable_cuda=False)
+needed = ['Na', 'Nb', 'Nv']
 
 def isNaN(string):
     return string != string
@@ -21,106 +26,120 @@ def find_noun(seg_list, pos_list):
 
     return tmp
 
-ws = WS('./tfidf/data', disable_cuda=False)
-pos = POS('./tfidf/data', disable_cuda=False)
-needed = ['Na', 'Nb', 'Nv']
-        
-data = pd.read_excel('./data/folder_nar/103-110_full.xlsx')
-clean_seg = []
-
-
-# ch keywords
-start = time.time()
-
-for idx, row in data.iterrows():
-    if isNaN(row['系統編號']) == False:
-        if type(row['計畫重點描述']) == str:
-            sen = row['計畫重點描述'].replace('_x000D_', '\n')
-            sen = sen.split('\n')
-        
-            seg_list = ws(sen)
-            pos_list = pos(seg_list)
-            tmp = ''
+def main(args):
+    data = pd.read_excel(args.data)
+    clean_seg = []
     
-            for s, p in zip(seg_list, pos_list):
-                if find_noun(s, p) != '':
-                    tmp += find_noun(s, p)
-                    
-            clean_seg.append(tmp)
+    # ch keywords
+    start = time.time()
 
-end = time.time()
-print(f'pos tagging and tokenization: {end-start} s')
+    for idx, row in data.iterrows():
+        if isNaN(row['系統編號']) == False:
+            if type(row['計畫重點描述']) == str:
+                sen = row['計畫重點描述'].replace('_x000D_', '\n')
+                sen = sen.split('\n')
+            
+                seg_list = ws(sen)
+                pos_list = pos(seg_list)
+                tmp = ''
+        
+                for s, p in zip(seg_list, pos_list):
+                    if find_noun(s, p) != '':
+                        tmp += find_noun(s, p)
+                        
+                clean_seg.append(tmp)
+
+    end = time.time()
+    print(f'pos tagging and tokenization: {end-start} s')
 
 
-# tfidf
-start = time.time()
+    # tfidf
+    start = time.time()
 
-vec = TfidfVectorizer(smooth_idf=True)
-score = vec.fit_transform(clean_seg)
-result = pd.DataFrame(score.toarray(), columns=vec.get_feature_names_out())
+    vec = TfidfVectorizer(smooth_idf=True)
+    score = vec.fit_transform(clean_seg)
+    result = pd.DataFrame(score.toarray(), columns=vec.get_feature_names_out())
 
-keywords = []
-eng_keywords = []
-black_list = ['產業', '應用', '平台', '業者', '資料', '領域', '成果', '檢查', '會議', '研究', '議題', '人員', '成效',
-              '政府', '團隊', '系統', '技術', '計畫', '任務', '事件', '用戶', '研討會', '法人', '貢獻', '單位', '行為',
-              '國家', '標準', '廠商', '大型', '設施', '產出', '執行', '計畫書', '經費', '項目', '發展', '分工', '效益',
-              '需要', '方面', '成果', '用戶']
+    keywords = []
+    eng_keywords = []
+    black_list = ['產業', '應用', '平台', '業者', '資料', '領域', '成果', '檢查', '會議', '研究', '議題', '人員', '成效',
+                '政府', '團隊', '系統', '技術', '計畫', '任務', '事件', '用戶', '研討會', '法人', '貢獻', '單位', '行為',
+                '國家', '標準', '廠商', '大型', '設施', '產出', '執行', '計畫書', '經費', '項目', '發展', '分工', '效益',
+                '需要', '方面', '成果', '用戶']
 
-for idx, row in result.iterrows():
-    tmp = []
+    for idx, row in result.iterrows():
+        tmp = []
 
-    for j in range(len(row)):
-        if (row[j] > 0.1) and (row.index[j] not in black_list):
-            tmp.append(row.index[j])
+        for j in range(len(row)):
+            if (row[j] > 0.1) and (row.index[j] not in black_list):
+                tmp.append(row.index[j])
+        
+        keywords.append(tmp)
+
+    end = time.time()
+    print(f'tfidf: {end-start} s')
+
+
+    # eng keywords
+    start = time.time()
+
+    eng = re.compile(r'[0-9A-Za-z_&²/]+')
+    other = re.compile(r'^[0-9_&²/]+$')
+
+    for idx, row in data.iterrows():
+        if isNaN(row['系統編號']) == False:
+            if type(row['計畫重點描述']) == str:
+                tmp = eng.findall(row['計畫重點描述'].replace('_x000D_', '\n'))
+                others = []
+                final1 = []
+                final2 = []
+
+                for t in tmp:
+                    if re.match(other, t):
+                        others.append(t)
+
+                final1 = [x for x in tmp if x not in others]
+
+                for x in final1:
+                    if x not in final2:
+                        final2.append(x)
+
+                eng_keywords.append(final2)
+
+    end = time.time()
+    print(f'eng keywords: {end-start} s')
+
+
+    # write json
+    all = dict()
+
+    for idx, row in data.iterrows():
+        if isNaN(row['系統編號']) == False:
+            d = dict()
+            d['code'] = row['系統編號']
+            d['name'] = row['計畫完整中文名稱']
+            d['tfidf'] = {'EN': eng_keywords[idx], 'CH': keywords[idx]}
+
+            all[row['系統編號']] = d
+
+    f = open(args.keyword, 'w', encoding='utf8')
+    json.dump(all, f, indent=4, ensure_ascii=False)
+    f.close()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('--data',
+                        default='./data/folder_nar/103-110_full.xlsx',
+                        type=str,
+                        dest='data',
+                        help='path to data')
+    parser.add_argument('--keyword',
+                        default='./data/folder_nar/tfidf.json',
+                        type=str,
+                        dest='keyword',
+                        help='path to save keyword file')
     
-    keywords.append(tmp)
-
-end = time.time()
-print(f'tfidf: {end-start} s')
-
-
-# eng keywords
-start = time.time()
-
-eng = re.compile(r'[0-9A-Za-z_&²/]+')
-other = re.compile(r'^[0-9_&²/]+$')
-
-for idx, row in data.iterrows():
-    if isNaN(row['系統編號']) == False:
-        if type(row['計畫重點描述']) == str:
-            tmp = eng.findall(row['計畫重點描述'].replace('_x000D_', '\n'))
-            others = []
-            final1 = []
-            final2 = []
-
-            for t in tmp:
-                if re.match(other, t):
-                    others.append(t)
-
-            final1 = [x for x in tmp if x not in others]
-
-            for x in final1:
-                if x not in final2:
-                    final2.append(x)
-
-            eng_keywords.append(final2)
-
-end = time.time()
-print(f'eng keywords: {end-start} s')
-
-
-# write json
-all = dict()
-
-for idx, row in data.iterrows():
-    if isNaN(row['系統編號']) == False:
-        d = dict()
-        d['code'] = row['系統編號']
-        d['name'] = row['計畫完整中文名稱']
-        d['tfidf'] = {'EN': eng_keywords[idx], 'CH': keywords[idx]}
-
-        all[row['系統編號']] = d
-
-f = open('./data/folder_nar/tfidf.json', 'w', encoding='utf8')
-json.dump(all, f, indent=4, ensure_ascii=False)
-f.close()
+    args = parser.parse_args()
+    main(args)
--- wordcloud/word_cloud.py
+++ wordcloud/word_cloud.py
@@ -1,63 +1,92 @@
 import pandas as pd
 import json
+import time
 from wordcloud import WordCloud, STOPWORDS
+from argparse import ArgumentParser
         
 def isNaN(string):
     return string != string
 
-data = pd.read_excel('./data/folder_nar/103-110_full.xlsx')
-
-with open('./data/folder_nar/tfidf.json', 'r') as f:
-    tfidf = json.load(f)
-
-class_name = ['AI、雲端、巨量資料', '虛擬實境', '人才補助與延攬',  '工研院與A+', '中小（堅）企業輔導', 
-              '化工材料與循環經濟', '能源', '太空/衛星', '文化', '水資源管理', '生技醫藥與精準醫療',
-              '產業人才培育', '自動駕駛', '災害防救', '物聯網', '網路通訊/4G/5G/5G+/6G', '防疫',
-              '創新創業', '資安', '長照與社會福利', '政策規劃與推動', '科研計畫補助', '科研設施',
-              '晶片半導體', '海洋科技', '食品安全與食品科技', '校園人才培育', '氣候變遷與減碳', '農業',
-              '國家標準檢驗', '國防', '專利與智財', '設計', '智慧交通', '智慧農業', '電商與物流', '智慧製造',
-              '智慧醫療', '智慧顯示器', '傳統產業高值化', '運輸載具', '園區管理', '農業生技', '運動科技',
-              '數位內容', '數位學習', '數位轉型與智慧政府', '環境監測與污染防治', '職安', '警政與法務']
-
-font_path = './wordcloud/NotoSansTC-Regular.otf'
-i = 0
-
-for c in class_name:
-    tfidf_str = ''
+def main(args):
+    start = time.time()
     
-    for idx, row in data.iterrows():
-        if isNaN(row['系統編號']) == False:
-            if data.iloc[idx]['新標籤'] == c:
-                for t in tfidf:
-                    for value in t.values():
-                        if value['code'] == data.iloc[idx]['系統編號']:
+    data = pd.read_excel(args.data)
+    with open(args.keyword, 'r') as f:
+        tfidf = json.load(f)
+
+    class_name = ['AI、雲端、巨量資料', '虛擬實境', '人才補助與延攬',  '工研院與A+', '中小（堅）企業輔導', 
+                '化工材料與循環經濟', '能源', '太空/衛星', '文化', '水資源管理', '生技醫藥與精準醫療',
+                '產業人才培育', '自動駕駛', '災害防救', '物聯網', '網路通訊/4G/5G/5G+/6G', '防疫',
+                '創新創業', '資安', '長照與社會福利', '政策規劃與推動', '科研計畫補助', '科研設施',
+                '晶片半導體', '海洋科技', '食品安全與食品科技', '校園人才培育', '氣候變遷與減碳', '農業',
+                '國家標準檢驗', '國防', '專利與智財', '設計', '智慧交通', '智慧農業', '電商與物流', '智慧製造',
+                '智慧醫療', '智慧顯示器', '傳統產業高值化', '運輸載具', '園區管理', '農業生技', '運動科技',
+                '數位內容', '數位學習', '數位轉型與智慧政府', '環境監測與污染防治', '職安', '警政與法務']
+
+    font_path = args.font
+    i = 0
+
+    for c in class_name:
+        tfidf_str = ''
+        
+        for idx, row in data.iterrows():
+            if isNaN(row['系統編號']) == False:
+                if row['新標籤'] == c:
+                    for key, value in tfidf.items():
+                        if key == row['系統編號']:
                             for ch in value['tfidf']['CH']:                   
                                 tfidf_str += ch + ' '
 
-    print(c)
+        if tfidf_str != '':
+            print(c)
+            wordcloud = WordCloud(background_color='white', contour_width=1, contour_color='steelblue', font_path=font_path).generate(tfidf_str)
+            wordcloud.to_file(args.wordcloud + str(i) + '.png')
+        
+        i += 1
 
+    # use the code below if no data is in a specific class
+    tfidf_str = ''
+
+    for idx, row in data.iterrows():
+        if isNaN(row['系統編號']) == False:
+            if isinstance(row['中文關鍵詞'], str):
+                if '虛擬實境' in row['中文關鍵詞']:
+                    for key, value in tfidf.items():
+                        if key == row['系統編號']:
+                            for ch in value['tfidf']['CH']:                   
+                                tfidf_str += ch + ' '
 
     if tfidf_str != '':
         wordcloud = WordCloud(background_color='white', contour_width=1, contour_color='steelblue', font_path=font_path).generate(tfidf_str)
-        wordcloud.to_file('./pea-sys/src/data/wordcloud/' + str(i) + '.png')
+        wordcloud.to_file(args.wordcloud + '1.png')
+
+    end = time.time()
+    print(f'wordcloud: {end-start} s')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('--data',
+                        default='./data/folder_nar/103-110_full.xlsx',
+                        type=str,
+                        dest='data',
+                        help='path to data')
+    parser.add_argument('--keyword',
+                        default='./data/folder_nar/tfidf.json',
+                        type=str,
+                        dest='keyword',
+                        help='path to keyword file')
+    parser.add_argument('--font',
+                        default='./wordcloud/NotoSansTC-Regular.otf',
+                        type=str,
+                        dest='font',
+                        help='path to font file')
+    parser.add_argument('--wordcloud',
+                        default='./pea-sys/src/data/wordcloud/',
+                        type=str,
+                        dest='wordcloud',
+                        help='path to save wordcloud images')
     
-    i += 1
-
-
-# use the code below if no data is in a specific class
-# tfidf_str = ''
-
-# for idx, row in data.iterrows():
-#     if isNaN(row['系統編號']) == False:
-#         if isinstance(data.iloc[i]['中文關鍵詞'], str):
-#             if '虛擬實境' in data.iloc[i]['中文關鍵詞']:
-#                 for t in tfidf:
-#                     if t['code'] == data.iloc[i]['系統編號']:
-#                         for ch in t['tfidf']['CH']:                   
-#                             tfidf_str += ch + ' '
-
-
-# if tfidf_str != '':
-#     wordcloud = WordCloud(background_color='white', contour_width=1, contour_color='steelblue', font_path=font_path).generate(tfidf_str)
-#     wordcloud.to_file('./pea-sys/src/data/wordcloud/1.png')
-#     print('save succeed')
+    args = parser.parse_args()
+    main(args)
