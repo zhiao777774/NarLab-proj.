eba83227e9dd67ee990681be86c8f2893268e53e
Mark Hsu <zhiaohsu@gmail.com>
2023-03-17 06:22:46 +0800

    update: change db connection method

--- pea-sys/api/database.py
+++ pea-sys/api/database.py
@@ -1,38 +1,21 @@
 import pymongo
 from typing import Union
-from ssh_pymongo import MongoSession
-
-from utils import get_env
 
 
 class MongoDB:
-    HOST = get_env('DB_HOST')  # local host
-    PORT = int(get_env('DB_PORT'))  # mongodb server port
-    SSH_HOST = get_env('DB_SSH_HOST')
-    SSH_PORT = int(get_env('DB_SSH_PORT'))
-    USER = get_env('DB_USER')  # student id.
-    PASSWORD = get_env('DB_PASSWORD')  # ssh password to machine 9102
-
     def __init__(self):
-        self.session: MongoSession = None
+        self.session: pymongo.MongoClient = None
         self.db: pymongo.database.Database = None
 
     def __getitem__(self, col_name: str) -> pymongo.collection.Collection:
         return self.db[col_name]
 
     def connect(self, db_name: str) -> None:
-        self.session = MongoSession(
-            host=self.SSH_HOST,
-            port=self.SSH_PORT,
-            user=self.USER,
-            password=self.PASSWORD,
-            uri=f'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT',
-        )
-
-        self.db = self.session.connection[db_name]
+        self.session = pymongo.MongoClient('mongodb://root:root@localhost:27017/?authMechanism=DEFAULT')
+        self.db = self.session[db_name]
 
     def close(self) -> None:
-        self.session.stop()
+        self.session.close()
 
     def find(self, col_name: str, condition: dict) -> list:
         return list(self.db[col_name].find(condition))
@@ -51,5 +34,5 @@ class MongoDB:
 if __name__ == '__main__':
     db_connection = MongoDB()
     db_connection.connect('nar')
-    print(db_connection.db.collection_names())
+    print(db_connection.db.list_collection_names())
     db_connection.close()
