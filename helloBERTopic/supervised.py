ce88f1e6b74597d663e53766365ca98422c4bb92
zhiao777774 <zhiaohsu@gmail.com>
2023-04-09 20:29:52 +0800

    update: change categories to CONSTANT

--- helloBERTopic/supervised.py
+++ helloBERTopic/supervised.py
@@ -2,12 +2,13 @@ import sys, os
 import numpy as np
 import pandas as pd
 import heapq
-from dataset import load_data
-from utils import DATA_PATH, EXPORT_PATH
+from helloBERTopic.dataset import load_data
+from helloBERTopic.utils import DATA_PATH, EXPORT_PATH
 from bertopic import BERTopic
 from halo import Halo
 from ckiptagger import construct_dictionary, WS
 from argparse import ArgumentParser
+# from sentence_transformers import SentenceTransformer
 
 sys.path.append(os.getcwd())
 from nar.util import XML_PATH
@@ -31,14 +32,67 @@ def find_class(data_df):
 
 def main(data_df):
 
-    label = find_class(data_df)
+    # label = find_class(data_df)
+    label =  [
+        'AI、雲端、巨量資料',
+        '虛擬實境',
+        '人才補助與延攬',
+        '工研院與A+',
+        '中小（堅）企業輔導',
+        '化工材料與循環經濟',
+        '能源',
+        '太空/衛星',
+        '太空/衛星',
+        '文化',
+        '水資源管理',
+        '生技醫藥與精準醫療',
+        '產業人才培育',
+        '自動駕駛',
+        '災害防救',
+        '物聯網',
+        '網路通訊/4G/5G/5G+/6G',
+        '防疫',
+        '創新創業',
+        '資安',
+        '長照與社會福利',
+        '政策規劃與推動',
+        '科研計畫補助',
+        '科研設施',
+        '晶片半導體',
+        '海洋科技',
+        '食品安全與食品科技',
+        '校園人才培育',
+        '氣候變遷與減碳',
+        '農業',
+        '國家標準檢驗',
+        '國防',
+        '專利與智財',
+        '設計',
+        '智慧交通',
+        '智慧農業',
+        '電商與物流',
+        '智慧製造',
+        '智慧醫療',
+        '智慧顯示器',
+        '傳統產業高值化',
+        '運輸載具',
+        '園區管理',
+        '農業生技',
+        '運動科技',
+        '數位內容',
+        '數位學習',
+        '數位轉型與智慧政府',
+        '環境監測與污染防治',
+        '職安',
+        '警政與法務'
+    ]
     total_X, y, timestamps, y_name = load_data(data_df, one_class=True)
     total_y = np.array(y)
 
     ws = WS(str(DATA_PATH))
 
     # 取出斷詞關鍵字
-    keysfile = DATA_PATH / "keys_v2.txt"
+    keysfile = f"{DATA_PATH}/keys_v2.txt"
     with open(keysfile) as file:
         lines = file.read().splitlines() 
 
@@ -65,7 +119,7 @@ def main(data_df):
     ws = [ " ".join(w) for w in word_sentence_list]
     #print(colored(f"BERTopics Input Showcase: \n [ { ws[0] }]", 'blue'))
 
-
+    # model = SentenceTransformer("./data/paraphrase-multilingual-MiniLM-L12-v2")
     topic_model = BERTopic(language = 'chinese (traditional)', verbose=True, nr_topics=51)
     topics, _ = topic_model.fit_transform(ws, y=total_y)
 
@@ -135,9 +189,9 @@ def main(data_df):
     )
 
     # 儲存成 html 檔案，供前端展示使用
-    bar_fig.write_html("./pea-sys/public/bar_fig.html")
-    topic_fig.write_html("./pea-sys/public/topic_fig.html")
-    tot_fig.write_html("./pea-sys/public/tot_fig.html")
+    bar_fig.write_html("/var/www/html/bar_fig.html")
+    topic_fig.write_html("/var/www/html/topic_fig.html")
+    tot_fig.write_html("/var/www/html/tot_fig.html")
 
 
 if __name__ == '__main__':
--- wordcloud/word_cloud.py
+++ wordcloud/word_cloud.py
@@ -27,7 +27,60 @@ def find_class(data):
 def main(data, tfidf, wc):
     start = time.time()
 
-    class_name = find_class(data)
+    # class_name = find_class(data)
+    class_name =  [
+        'AI、雲端、巨量資料',
+        '虛擬實境',
+        '人才補助與延攬',
+        '工研院與A+',
+        '中小（堅）企業輔導',
+        '化工材料與循環經濟',
+        '能源',
+        '太空/衛星',
+        '太空/衛星',
+        '文化',
+        '水資源管理',
+        '生技醫藥與精準醫療',
+        '產業人才培育',
+        '自動駕駛',
+        '災害防救',
+        '物聯網',
+        '網路通訊/4G/5G/5G+/6G',
+        '防疫',
+        '創新創業',
+        '資安',
+        '長照與社會福利',
+        '政策規劃與推動',
+        '科研計畫補助',
+        '科研設施',
+        '晶片半導體',
+        '海洋科技',
+        '食品安全與食品科技',
+        '校園人才培育',
+        '氣候變遷與減碳',
+        '農業',
+        '國家標準檢驗',
+        '國防',
+        '專利與智財',
+        '設計',
+        '智慧交通',
+        '智慧農業',
+        '電商與物流',
+        '智慧製造',
+        '智慧醫療',
+        '智慧顯示器',
+        '傳統產業高值化',
+        '運輸載具',
+        '園區管理',
+        '農業生技',
+        '運動科技',
+        '數位內容',
+        '數位學習',
+        '數位轉型與智慧政府',
+        '環境監測與污染防治',
+        '職安',
+        '警政與法務'
+    ]
     font = './wordcloud/NotoSansTC-Regular.otf'
     i = 0
 
@@ -63,7 +116,7 @@ def main(data, tfidf, wc):
 
     if tfidf_str != '':
         wordcloud = WordCloud(background_color='white', contour_width=1, contour_color='steelblue', font_path=font).generate(tfidf_str)
-        wordcloud.to_file(args.wordcloud + '1.png')
+        wordcloud.to_file(wc + '1.png')
 
     end = time.time()
     print(f'wordcloud: {end-start} s')
